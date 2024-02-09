7d2dbf6c04b7b5a0b43cbb486784e563092211cd
Gustavo Ribeiro <gustaavoribeeiro@hotmail.com>
2024-02-09 13:19:23 -0300

    Update crud_user.py

--- app/crud/crud_user.py
+++ app/crud/crud_user.py
@@ -2,9 +2,9 @@ from typing import Any, Dict, Optional, Union
 
 from sqlalchemy.orm import Session
 
-from app.core.security import get_password_hash, verify_password
+from app.services.security import get_password_hash, verify_password
 from app.crud.base import CRUDBase
-from app.models import User
+from app.db.models import User
 from app.schemas.user import UserCreate, UserUpdate
 
 
